3b15241f693963c70d525179189617cc20f17588
taron <taron@SPACE-STATION>
2023-09-13 12:06:17 -0400

    another connection string change

--- truthfulls.com/Services/StockDataRetrievalService.cs
+++ truthfulls.com/Services/StockDataRetrievalService.cs
@@ -24,7 +24,7 @@ namespace truthfulls.com.Services
             this.AllTickerNamesCache = "Tickers";
             this.Builder = new();
 
-            this.Builder.ConnectionString = config.GetConnectionString("default");
+            this.Builder.ConnectionString = config.GetConnectionString("CUSTOMCONNSTR_default");
         }
 
         //for testing

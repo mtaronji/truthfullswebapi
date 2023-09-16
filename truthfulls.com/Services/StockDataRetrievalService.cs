d3f8900ed66761c6e9e39cc6ca452dc1a5a76da0
taron <taron@SPACE-STATION>
2023-09-15 23:50:30 -0400

    configured env

--- truthfulls.com/Services/StockDataRetrievalService.cs
+++ truthfulls.com/Services/StockDataRetrievalService.cs
@@ -23,7 +23,7 @@ namespace truthfulls.com.Services
             this.CachedTickers = new List<string>() { };
             this.AllTickerNamesCache = "Tickers";
             this.Builder = new();
-            var con = config.GetConnectionString("CUSTOMCONNSTR_stock");
+            var con = config.GetConnectionString("stock");
             this.Builder.ConnectionString = con;
         }
 
